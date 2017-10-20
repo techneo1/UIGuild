0c98de2c03384ff19f0c10a4a7b53cb8220335c3
Srikanth Shet <techneo1@gmail.com>
2017-10-21 01:20:34 +0530

    Adding Babel loader for ES6 compilation

--- webpack/build/bundle.js
+++ webpack/build/bundle.js
@@ -67,14 +67,22 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const sum = __webpack_require__(1);
+"use strict";
+
+
+var sum = __webpack_require__(1);
 console.log(sum(5, 10));
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-const sum = (x, y) => x + y;
+"use strict";
+
+
+var sum = function sum(x, y) {
+  return x + y;
+};
 
 module.exports = sum;
 
