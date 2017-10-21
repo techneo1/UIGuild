bb16a9354fffffd8781efbd8d9c4caeae3aa7183
Srikanth Shet <techneo1@gmail.com>
2017-10-21 13:16:24 +0530

    Handling images

--- webpack/src/image_viewer.js
+++ webpack/src/image_viewer.js
@@ -1,6 +1,13 @@
+import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
+
 import '../styles/image_viewer.css';
 window.onload = function(){
-    const image = document.createElement('img');
-    image.src = 'http://lorempixel.com/400/400';
-    document.body.appendChild(image);
+    const smallImage = document.createElement('img');
+    smallImage.src = small;
+    document.body.appendChild(smallImage);
+
+    const bigImage = document.createElement('img');
+    bigImage.src = big;
+    document.body.appendChild(bigImage);
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,7 +5,8 @@ const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'build/'
     },
     module: {
         rules: [
@@ -19,6 +20,16 @@ const config = {
                     use: "css-loader"
                 }),
                 test: /\.css$/          
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {limit: 40000}
+                    },
+                    'image-webpack-loader'
+                ]
             }
         ]
     },
