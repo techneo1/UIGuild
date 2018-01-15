f74db89a9c2e1b892352f5193c1c89a23cc38fb0
Srikanth Shet <s.shet@prowareness.com>
2018-01-15 18:00:14 +0530

    Databinding - String Interpolation

--- angular/src/app/server/server.component.ts
+++ angular/src/app/server/server.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
-
 })
 export class ServerComponent {
+    serverID: number = 10;
+    serverStatus: string = 'offline';
 
+    getServerStatus(): string {
+        return this.serverStatus;
+    }
 }
