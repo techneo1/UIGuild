e0c53d13337cae4ce34576e56d25201000402c5f
Srikanth Shet <s.shet@prowareness.com>
2018-01-16 12:29:36 +0530

    Assignment 3 - Directives

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -8,9 +8,21 @@ import { Component } from '@angular/core';
     h3 {
       color: orange;
     }
+
+    .white-text {
+        color: white;
+    }
   `]
 })
 export class AppComponent {
     title = 'app';
     username = '';
+    showDetails = false;
+    logMessages = [];
+
+    onToggleDisplyDetails(event){
+        console.log(event);
+        this.showDetails = !this.showDetails;
+        this.logMessages.push(event.timeStamp);
+    }
 }
