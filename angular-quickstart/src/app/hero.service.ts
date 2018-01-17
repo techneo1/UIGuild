53c587fd45117127b792cdabe8aa415472aefbaf
Srikanth Shet <s.shet@prowareness.com>
2018-01-17 16:35:13 +0530

    Initial Commit

--- angular-quickstart/src/app/hero.service.ts
+++ angular-quickstart/src/app/hero.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { Heroes } from './mock-heroes';
 
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/Observable/of';
+
+import {MessageService} from './message.service';
+
 // Services can be provided Child Component, Application Shell or App Module
 @Injectable()
 export class HeroService {
   // Can get data from a web service, local storage or a mock data source.
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-      return Heroes;
+  // HttpClient.get<Hero[]> 
+  getHeroes(): Observable<Hero[]> {
+      return of(Heroes);
   }
 
 }
