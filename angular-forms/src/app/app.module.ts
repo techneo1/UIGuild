745b490b69f636291169b2a18c820d014b5c54d3
Srikanth Shet <s.shet@prowareness.com>
2018-01-23 18:03:44 +0530

    3. Get user input from a template reference variable

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ClickMeComponent } from './user-input/click-me/click-me.component';
 import { KeyupComponent } from './user-input/keyup/keyup.component';
+import { LoopBackComponent } from './user-input/loop-back/loop-back.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ClickMeComponent,
-    KeyupComponent
+    KeyupComponent,
+    LoopBackComponent
   ],
   imports: [
     BrowserModule
