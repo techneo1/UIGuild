8955e374e5e47c81c725f59f9983e4593ce38ab7
Srikanth Shet <s.shet@prowareness.com>
2018-01-23 17:04:45 +0530

    Initial commit

--- angular-quickstart/src/app/heroes/heroes.component.ts
+++ angular-quickstart/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { Heroes } from '../mock-heroes';
-import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,26 +8,18 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
-  heroes: Hero[];
+  heroes: Hero[] = Heroes;
   selectedHero: Hero;
   
-  // Simple Initialisation such as wiring constructor parameters to properties. 
-  constructor(private heroService: HeroService) { 
-
-  }
+  constructor() { }
 
   // Initialization logic 
   ngOnInit() {
-     this.getHeroes();
+     
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  getHeroes(): void {
-      this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
 }
